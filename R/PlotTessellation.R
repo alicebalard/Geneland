06fc1f03eecb11fa98f5a9c4481139908f7b459d
alice <alice.cam.balard@gmail.com>
2019-08-31 16:20:42 +0200

    Add new colors and cities names to PlotTessellation.R

--- R/PlotTessellation.R
+++ R/PlotTessellation.R
@@ -1,5 +1,5 @@
 PlotTessellation <-
-function (coordinates, path.mcmc, printit = FALSE, path) 
+function (coordinates, path.mcmc, printit = FALSE, path, colfancy = heat.colors(100), plotCities = F)) 
 {
     coordinates <- as.matrix(coordinates)
     param.postprocess <- as.matrix(read.table(paste(path.mcmc, 
@@ -20,7 +20,10 @@ function (coordinates, path.mcmc, printit = FALSE, path)
             2] - delta.coord/2), max(coordinates[, 2] + delta.coord/2), 
             length = nydom), matrix(dom.post[, iclass], nrow = nxdom, 
             ncol = nydom, byrow = TRUE), xlab = "x coordinates", 
-            ylab = "y coordinates", asp = 1)
+            ylab = "y coordinates", asp = 1, col = colfancy)
+        ggmap(get_stamenmap())+
+            scale_x_continuous( limits = c(min(coordinates[,1]), max(coordinates[,1])), expand = c(0,0))+
+            scale_y_continuous( limits = c(min(coordinates[,2]), max(coordinates[,2])) , expand = c(0,0))
         title(main = paste("Map of posterior probability to belong to cluster ", 
             iclass))
         contour(seq(min(coordinates[, 1] - delta.coord/2), max(coordinates[, 
@@ -28,7 +31,12 @@ function (coordinates, path.mcmc, printit = FALSE, path)
             2] - delta.coord/2), max(coordinates[, 2] + delta.coord/2), 
             length = nydom), matrix(dom.post[, iclass], nrow = nxdom, 
             ncol = nydom, byrow = TRUE), add = TRUE)
-        points(coordinates[, 1], coordinates[, 2], pch = 16)
+        points(coordinates[, 1], coordinates[, 2], pch = 21, bg = "grey", col = "black", cex = 1)
+        # Add cities locations and names, and scale, if wanted
+        if(plotCities == TRUE){
+            map.cities(country="Germany", label=T, minpop=25000, cex = 1.5, capitals = c(0,1), col = "white")
+            map.scale(ratio = F, col = "white", linecol = "white")
+        }
         if (printit == TRUE) {
             postscript(file = paste(path, "/mapproba.pop", iclass, 
                 ".ps", sep = ""))
